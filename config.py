2233180d2e9e424b1fca486dd12fc200af2cc0a9
pjy4183 <pjy4183@gmail.com>
2020-05-07 01:40:29 -0500

    Update

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '')
-CHAT_ID = os.environ.get('CHAT_ID','')
+CHAT_ID = os.environ.get('CHAT_ID', '')
 
 if not TELEGRAM_TOKEN or not CHAT_ID:
-    raise Exception('TELEGRAM_TOKEN, CHAT_ID 확인 필요')
+  raise Exception('TELEGRAM_TOKEN, CHAT_ID 확인필요')
 
 if __name__ == "__main__":
-    print(TELEGRAM_TOKEN)
-    print(CHAT_ID)
+  print(TELEGRAM_TOKEN)
+  print(CHAT_ID)
--- noti.py
+++ noti.py
@@ -1,8 +1,8 @@
 from config import TELEGRAM_TOKEN, CHAT_ID
 import requests as rq
-import telegram
+import telegram 
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 def send(t):
-    bot.sendMessage(CHAT_ID, t, parse_mode=telegram.ParseMode.HTML)
+  bot.sendMessage(CHAT_ID, t, parse_mode=telegram.ParseMode.HTML)
